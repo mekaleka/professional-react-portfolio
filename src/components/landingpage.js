f483aa2bc3b4553b45d270c168e206ff91fecbe5
mekaleka <www.no-reply.com>
2020-04-07 16:53:55 -0700

    updated landing page with styling and image

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -6,7 +6,13 @@ class Landing extends Component {
     return (
       <div style={{width: "100%", margin: "auto"}}>
           <Grid className="landing-grid">
-            <Cell col={12}></Cell>
+            <Cell col={12}>
+              <img
+              src="https://avatars3.githubusercontent.com/u/58678985?s=460&u=aa7e88c92eff7ce64d65228342241a9ccc5f07be&v=4"
+              alt="profilepic"
+              className="profile-image"
+              />
+            </Cell>
           </Grid>
       </div>
     );

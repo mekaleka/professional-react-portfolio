f017a17971d4433d354b51828947c41e04cadd68
mekaleka <www.no-reply.com>
2020-04-06 23:16:31 -0700

    Updates

---
+++ precache-manifest.8c186f57959567f20abd48feb37f08e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec1d36d7f8a91ca29957136c8ed2e65",
+    "url": "/professional-react-portfolio/index.html"
+  },
+  {
+    "revision": "61ec16e0b5a53c930c33",
+    "url": "/professional-react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "956ac1df34cb4db1e8f7",
+    "url": "/professional-react-portfolio/static/js/2.29755ca8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/professional-react-portfolio/static/js/2.29755ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ec16e0b5a53c930c33",
+    "url": "/professional-react-portfolio/static/js/main.135c71d1.chunk.js"
+  },
+  {
+    "revision": "ea34232610a7d463eceb",
+    "url": "/professional-react-portfolio/static/js/runtime-main.22f4b090.js"
+  }
+]);

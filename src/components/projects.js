984192365dc32fcc31cef1554ce60db67781eccc
mekaleka <www.no-reply.com>
2020-04-07 23:12:38 -0700

    created card template for projects to be displayed

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,45 +1,72 @@
 import React, { Component } from "react";
-import { Tabs, Tab } from "react-mdl";
+import { Tabs, Tab, Grid, Cell, Card, CardTitle,CardActions, Button, CardMenu, IconButton, CardText } from "react-mdl";
 
 class Projects extends Component {
   constructor(props) {
     super(props);
-    this.state= {activeTab: 0};
+    this.state = { activeTab: 0 };
   }
 
   toggleCategories() {
-
-  if(this.state.activeTab === 0) {
-    return(
-      <div><h1>This is React</h1></div>
-    )
-  } else if(this.state.activeTab === 1) {
-    return (
-      <div><h1>This is MongoDB</h1></div>
-    )
+    if (this.state.activeTab === 0) {
+      return (
+        <div>
+          <Card shadow={5} style={{minWidth: "450", margin: "auto"}}>
+            <CardTitle style={{color: "#fff", height: "175px", background: "url(https://user-images.githubusercontent.com/58678985/78611016-e124f580-781a-11ea-9bfc-ac34cb6b063f.png) center / cover"}}>React Project #1</CardTitle>
+            <CardText>
+            The React Employee Directory app allows for business professionals to access non-sensitive employee data.
+            </CardText>
+            <CardActions border>
+              <Button colored>GITHUB</Button>
+              <Button colored>DEMO</Button>
+            </CardActions>
+            <CardMenu style={{color: "#fff"}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is MongoDB</h1>
+        </div>
+      );
     } else if (this.state.activeTab === 2) {
       return (
-        <div><h1>This is JavaScript</h1></div>
-      )
-    }else if (this.state.activeTab === 3) {
+        <div>
+          <h1>This is JavaScript</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
       return (
-        <div><h1>This is jQuery</h1></div>
-      )
-    }else if (this.state.activeTab === 4) {
+        <div>
+          <h1>This is jQuery</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 4) {
       return (
-        <div><h1>This is Express.js</h1></div>
-      )
-    }else if (this.state.activeTab === 5) {
+        <div>
+          <h1>This is Express.js</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 5) {
       return (
-        <div><h1>This is MySQL</h1></div>
-      )
+        <div>
+          <h1>This is MySQL</h1>
+        </div>
+      );
     }
   }
 
   render() {
     return (
       <div className="category-tabs">
-        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={(tabId) => this.setState({ activeTab: tabId })}
+          ripple
+        >
           <Tab>React</Tab>
           <Tab>MongoDB</Tab>
           <Tab>JavaSript</Tab>
@@ -49,10 +76,14 @@ class Projects extends Component {
         </Tabs>
 
         <section className="projects-grid">
-          {this.toggleCategories()}
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
         </section>
       </div>
-    );
+    )
   }
 }
 

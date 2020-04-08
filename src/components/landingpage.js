d2542e61324fc1d6a556e73d28204263c4ec8d80
mekaleka <www.no-reply.com>
2020-04-07 22:36:43 -0700

    setup tabs for projects and their routes

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,26 +1,37 @@
 import React, { Component } from "react";
-import { Grid, Cell } from "react-mdl"
+import { Grid, Cell } from "react-mdl";
 
 class Landing extends Component {
   render() {
     return (
-      <div style={{width: "100%", margin: "auto"}}>
-          <Grid className="landing-grid">
-            <Cell col={12}>
-              <img
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
               src="https://avatars3.githubusercontent.com/u/58678985?s=460&u=aa7e88c92eff7ce64d65228342241a9ccc5f07be&v=4"
               alt="profilepic"
               className="profile-image"
-              />
-              <div className="title-text">
-                <h1>Junior Full Stack Web Developer</h1>
+            />
+            <div className="title-text">
+              <h1>Junior Full Stack Web Developer</h1>
 
-                <hr/>
+              <hr />
 
-                <p>HTML/CSS | Bootstrap |JavaScript | jQuery | Node.js | React | Express | MongoDB | MySQL</p>
+              <p>
+                HTML/CSS | Bootstrap |JavaScript | jQuery | Node.js | React |
+                Express | MongoDB | MySQL
+              </p>
+              <div className="media-links">
+                <a href="https://github.com/mekaleka" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+                <a href="https://www.linkedin.com/in/michael-plichta-60a391199/" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
               </div>
-            </Cell>
-          </Grid>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,56 @@
 import React, { Component } from "react";
+import { Tabs, Tab } from "react-mdl";
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {activeTab: 0};
+  }
+
+  toggleCategories() {
+
+  if(this.state.activeTab === 0) {
+    return(
+      <div><h1>This is React</h1></div>
+    )
+  } else if(this.state.activeTab === 1) {
+    return (
+      <div><h1>This is MongoDB</h1></div>
+    )
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div><h1>This is JavaScript</h1></div>
+      )
+    }else if (this.state.activeTab === 3) {
+      return (
+        <div><h1>This is jQuery</h1></div>
+      )
+    }else if (this.state.activeTab === 4) {
+      return (
+        <div><h1>This is Express.js</h1></div>
+      )
+    }else if (this.state.activeTab === 5) {
+      return (
+        <div><h1>This is MySQL</h1></div>
+      )
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Projects Page</h1>
+      <div className="category-tabs">
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+          <Tab>React</Tab>
+          <Tab>MongoDB</Tab>
+          <Tab>JavaSript</Tab>
+          <Tab>jQuery</Tab>
+          <Tab>Express.js</Tab>
+          <Tab>MySQL</Tab>
+        </Tabs>
+
+        <section className="projects-grid">
+          {this.toggleCategories()}
+        </section>
       </div>
     );
   }

2e6c7d0f53410f25fdfd3ff5ebe9bd40a8f239ad
mekaleka <www.no-reply.com>
2020-04-10 21:24:01 -0700

    adding home route

--- src/components/main.js
+++ src/components/main.js
@@ -9,11 +9,12 @@ import Projects from "./projects";
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={LandingPage} />
+    <Route exact  path="/professional-react-portfolio/" component={LandingPage} />
     <Route exact path="/aboutme" component={AboutMe} />
     <Route exact path="/resume" component={Resume} />
     <Route exact path="/contact" component={Contact} />
     <Route exact path="/projects" component={Projects} />
+  
   </Switch>
 );
 

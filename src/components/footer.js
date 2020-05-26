51c7e8c0ff8eeb24e81db79ef152d651768710b2
mekaleka <www.no-reply.com>
2020-05-26 05:55:06 -0700

    push to gh-pages

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,9 +4,7 @@ import { Layout, Footer, Navigation, Drawer, Content } from "react-mdl";
 class FooterBar extends React.Component {
   render() {
     return (
-      // <Layout>
         <Footer className="navbar-color footer-index" title="Michael J Plichta"></Footer>
-      // </Layout>
     );
   }
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,32 +3,29 @@ import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import { Link } from "react-router-dom";
 import Landingpage from "./landingpage";
 
-function Navbar () {
-    return (
-        <div className="demo-big-content">
-        {/* <Layout> */}
-          <Header className="navbar-color" title="Michael J Plichta" scroll>
-            <Navigation>
-              <Link to="/professional-react-portfolio">Home Page</Link>
-              <Link to="/aboutme">About Me</Link>
-              <Link to="/projects">Projects</Link>
-              <Link  to="/resume">Resume</Link>
-              <Link to="/contact">Contact</Link>
-            </Navigation>
-          </Header>
-          <Drawer title="Title">
-            <Navigation>
-              <Link to="/professional-react-portfolio">Home Page</Link>
-              <Link to="/aboutme">About Me</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/resume">Resume</Link>
-              <Link to="/contact">Contact</Link>
-            </Navigation>
-          </Drawer>
-     
-          {/* </Layout> */}
-          </div>
-    )
+function Navbar() {
+  return (
+    <div className="demo-big-content">
+      <Header className="navbar-color" title="Michael J Plichta" scroll>
+        <Navigation>
+          <Link to="/professional-react-portfolio">Home Page</Link>
+          <Link to="/aboutme">About Me</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/resume">Resume</Link>
+          <Link to="/contact">Contact</Link>
+        </Navigation>
+      </Header>
+      <Drawer title="Title">
+        <Navigation>
+          <Link to="/professional-react-portfolio">Home Page</Link>
+          <Link to="/aboutme">About Me</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/resume">Resume</Link>
+          <Link to="/contact">Contact</Link>
+        </Navigation>
+      </Drawer>
+    </div>
+  );
 }
 
 export default Navbar;
